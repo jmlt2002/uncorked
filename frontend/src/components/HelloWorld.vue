5c864741365a2ae4cfd93bcea735bbb45226ee6f
migalha <joao.miguel.migalha@gmail.com>
2025-10-12 21:45:30 +0100

    chore: add PrimeVue component library

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,41 +1,29 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
+    <IftaLabel>
+      <InputText id="usernameEmail" v-model="usernameEmail" variant="filled" />
+      <label for="usernameEmail">Email or username</label>
+    </IftaLabel>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <IftaLabel>
+      <Password id="password" v-model="password" variant="filled" />
+      <label for="password">Password</label>
+    </IftaLabel>
+
+    <Button label="Submit" class="btn"/>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
+<script setup lang="ts">
+import { ref } from 'vue';
+import { InputText, Password, IftaLabel, Button } from 'primevue';
+
+const usernameEmail = ref('');
+const password = ref('');
+</script>
+
 <style scoped>
-.read-the-docs {
-  color: #888;
+.btn {
+  width: 100%;
 }
 </style>
