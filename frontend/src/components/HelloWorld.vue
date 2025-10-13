e45aff18baa0dfe142febeb479e1c4f8eac7f4a0
migalha <joao.miguel.migalha@gmail.com>
2025-10-13 10:05:49 +0100

    feat: base login form

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,29 +1,62 @@
 <template>
-  <div>
-    <IftaLabel>
-      <InputText id="usernameEmail" v-model="usernameEmail" variant="filled" />
-      <label for="usernameEmail">Email or username</label>
-    </IftaLabel>
+  <div class="container">
+    <Form class="form">
+      <div class="form-field">
+        <InputText id="usernameEmail" v-model="usernameEmail" placeholder="Username or email" />
+      </div>
 
-    <IftaLabel>
-      <Password id="password" v-model="password" variant="filled" />
-      <label for="password">Password</label>
-    </IftaLabel>
+      <div class="form-field">
+        <Password id="password" v-model="password" placeholder="Password" fluid/>
+      </div>
 
-    <Button label="Submit" class="btn"/>
+      <Button label="Submit" class="form-field" />
+
+      <p>Not a member yet?<a href=""> Register here!</a></p>
+    </Form>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { InputText, Password, IftaLabel, Button } from 'primevue';
+import { InputText, Password, Button } from 'primevue';
+import { Form } from '@primevue/forms';
 
 const usernameEmail = ref('');
 const password = ref('');
 </script>
 
 <style scoped>
-.btn {
-  width: 100%;
+.container {
+  min-height: 100vh;
+  background-image: url('@/assets/cellar_blurred.jpg');
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
 }
+
+.form {
+  background-color: rgba(255, 255, 255, 0.9);
+  padding: 2rem;
+  border-radius: 10px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+  width: 50%;
+  box-sizing: border-box;
+  max-width: 400px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1em;
+}
+
+.form-field {
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+}
+
 </style>
